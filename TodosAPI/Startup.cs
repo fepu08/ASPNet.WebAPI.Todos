68b72065485ecc29c980f72fccc83fd5c98dbed8
fepu08 <ferencbesenyei@protonmail.com>
2021-06-18 07:57:49 +0200

    Register the database context as a service

--- TodosAPI/Startup.cs
+++ TodosAPI/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TodosAPI.Models;
 
 namespace TodosAPI
 {
@@ -27,6 +22,7 @@ namespace TodosAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
